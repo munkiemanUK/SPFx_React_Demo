d1c91db9b196ca78389946efd91f6c755ccc0b9d
Jason Clark <jclark@maximusuk.co.uk>
2022-01-07 17:03:39 +0000

    updated code with section 11 - chapter 62 code

--- src/webparts/reactDemo/components/ReactDemo.tsx
+++ src/webparts/reactDemo/components/ReactDemo.tsx
@@ -3,18 +3,35 @@ import styles from './ReactDemo.module.scss';
 import { IReactDemoProps } from './IReactDemoProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
+import * as $ from 'jquery';
 export interface IReactDemoState{
   stageTitle: string;
+  listItems: [
+    {
+      "Title" : "",
+      "ID" : "",
+      "Name" : ""
+    }
+  ]
 }
 
 export default class ReactDemo extends React.Component<IReactDemoProps, IReactDemoState> {
+  static siteURL: string="";
 
   public constructor(props: IReactDemoProps, state: IReactDemoState)
   {
     super(props);
     this.state = {
-      stageTitle: 'component Constructor has been called'
+      stageTitle: 'component Constructor has been called',
+      listItems:[
+        {
+          "Title" : "",
+          "ID" : "",
+          "Name" : ""          
+        }
+      ]
     };
+    ReactDemo.siteURL=this.props.absoluteurl;
     this.updateState = this.updateState.bind(this);
     console.log('Stage Title from Constructor : '+ this.state.stageTitle);
   }  
@@ -24,10 +41,24 @@ export default class ReactDemo extends React.Component<IReactDemoProps, IReactDe
   }
 
   public componentDidMount(): void {
+    
     console.log('stage title from componentDidMount : ' + this.state.stageTitle);
     this.setState({
       stageTitle:'componentDidMount has been called'
-    });  
+    });
+    
+    $.ajax({
+      url: `${ReactDemo.siteURL}/_api/web/lists/getbytitle/('PO_Request_Test)/items`,
+      type:'GET',
+      headers: {'Accept': 'application/json; odata=verbose'},
+      success: function(data){
+        this.setState({
+          listItems: data.d.results
+        });
+      },
+      error: function(jqHXR,textStatus,errorThrown){        
+      }
+    })
   }
 
   public updateState(){
